9f9a23dc7fe160e9c8df7c37493d0103941a1a94
Claus Polanka <claus.polanka@chello.at>
2012-10-26 19:04:29 +0200

    First test which fails because of UnknownHostException

---
+++ src/main/java/CurrencyConversion.java
@@ -0,0 +1,67 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CurrencyConversion {
+
+    public static BigDecimal convertFromTo(String fromCurrency, String toCurrency) {
+        Map<String, String> symbolToName = currencySymbols();
+        if (!symbolToName.containsKey(fromCurrency))
+            throw new IllegalArgumentException(String.format("Invalid from currency: %s", fromCurrency));
+        if (!symbolToName.containsKey(toCurrency))
+            throw new IllegalArgumentException(String.format("Invalid to currency: %s", toCurrency));
+
+        String url = String.format("http://www.gocurrency.com/v2/dorate.php?inV=1&from=%s&to=%s&Calculate=Convert", toCurrency, fromCurrency);
+
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpGet httpget = new HttpGet(url);
+            HttpResponse response = httpclient.execute(httpget);
+            HttpEntity entity = response.getEntity();
+            StringBuffer result = new StringBuffer();
+            if (entity != null) {
+                InputStream instream = entity.getContent();
+                InputStreamReader irs = new InputStreamReader(instream);
+                BufferedReader br = new BufferedReader(irs);
+                String l;
+                while ((l = br.readLine()) != null) {
+                    result.append(l);
+                }
+            }
+            String theWholeThing = result.toString();
+            int start = theWholeThing.lastIndexOf("<div id=\"converter_results\"><ul><li>");
+            String substring = result.substring(start);
+            int startOfInterestingStuff = substring.indexOf("<b>") + 3;
+            int endOfIntererestingStuff = substring.indexOf("</b>",
+                startOfInterestingStuff);
+            String interestingStuff = substring.substring(
+                startOfInterestingStuff, endOfIntererestingStuff);
+            String[] parts = interestingStuff.split("=");
+            String value = parts[1].trim().split(" ")[0];
+            BigDecimal bottom = new BigDecimal(value);
+            return bottom;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Map<String, String> currencySymbols() {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpGet httpget = new HttpGet("http://temporary");
+        try {
+            HttpResponse response = httpclient.execute(httpget);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return new HashMap<String, String>();
+    }
+}
---
+++ src/test/java/CurrencyConversionTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class CurrencyConversionTest {
+
+    @Test
+    public void returnExpectedConversion_v1() {
+        CurrencyConversion.convertFromTo("USD", "EUR");
+    }
+
+}
