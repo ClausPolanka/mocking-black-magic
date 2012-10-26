3959d8feed11e14dbfb8851d8c251e3d6d37a13e
Claus Polanka <claus.polanka@chello.at>
2012-10-26 19:18:14 +0200

    Third version which uses JMockIt to completely get rid of network dependency.

--- src/test/java/CurrencyConversionTest.java
+++ src/test/java/CurrencyConversionTest.java
@@ -1,18 +1,47 @@
 import mockit.NonStrictExpectations;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.math.BigDecimal;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 
 public class CurrencyConversionTest {
 
     @Test
-    public void returnExpectedConversion_v2() {
+    public void returnExpectedConversion_v3() throws Exception {
+        final ByteArrayInputStream bais = new ByteArrayInputStream(
+            "<div id=\"converter_results\"><ul><li><b>1 USD = 0.98 EUR</b>".getBytes());
+
+        new NonStrictExpectations() {
+            DefaultHttpClient httpclient;
+            HttpResponse response;
+            HttpEntity entity;
+
+            {
+                httpclient.execute((HttpUriRequest) any);
+                result = response;
+                response.getEntity();
+                result = entity;
+                entity.getContent();
+                result = bais;
+            }
+        };
+
         new NonStrictExpectations(CurrencyConversion.class) {{
             CurrencyConversion.currencySymbols();
             result = mapFrom("USD", "EUR");
         }};
-        CurrencyConversion.convertFromTo("USD", "EUR");
+        BigDecimal result = CurrencyConversion.convertFromTo("USD", "EUR");
+        assertThat(result.subtract(new BigDecimal(2)), is(lessThanOrEqualTo(new BigDecimal(0.001))));
     }
 
     private Map<String, String> mapFrom(String... keyValuePairs) {
